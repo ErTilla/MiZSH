812504b85344a365b86b38d55fb402120e2febcb
ErTilla <ErTilla69@gmail.com>
2024-04-24 09:17:34 +0200

    zsh

---
+++ zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Verificar si zsh está instalado
+zsh_path=$(which zsh)
+
+if [ "$zsh_path" == "/usr/bin/zsh" ]; then
+    echo "zsh ya está instalado"
+else
+    echo "zsh no está instalado"
+    # Aquí puedes agregar el código para instalar zsh si lo deseas
+    sudo apt install zsh
+     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
