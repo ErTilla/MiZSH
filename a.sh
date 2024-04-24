5530451b6075fc39c30229145a7b66eebfe1abe0
ErTilla <ErTilla69@gmail.com>
2024-04-24 11:22:52 +0200

    Pruebabat

--- a.sh
+++ a.sh
@@ -6,12 +6,30 @@ if which batcat &> /dev/null ; then
 
 else
     echo "batcat no está instalado en este sistema."
-    git clone https://github.com/sharkdp/bat.git
-    sudo apt update
-    sudo apt install build-essential cmake zlib1g-dev
+    sudo apt install bat
 
-    cd bat
-    cargo build --release
-    sudo cp target/release/bat /usr/local/bin/
-    bat --version
+    if which batcat &> /dev/null ; then
+        echo "batcat está instalado en este sistema."
+        batcat --version
+
+    else
+        echo "batcat no está instalado en este sistema (2 prueba)."
+        git clone https://github.com/sharkdp/bat.git
+        sudo apt update
+        sudo apt install build-essential cmake zlib1g-dev
+        cd bat
+        cargo build --release
+        sudo cp target/release/bat /usr/local/bin/
+        cd ../
+        bat --version
+        if which batcat &> /dev/null ; then
+            echo "batcat está instalado en este sistema."
+            batcat --version
+
+        else
+            echo "batcat no está instalado en este sistema (3 prueba)."
+            sudo rm -r ./bat
+
+        fi
+    fi
 fi
