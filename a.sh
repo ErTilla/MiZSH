89416642a2dcffbdf2956fbb20ddee81a53b6274
ErTilla <ErTilla69@gmail.com>
2024-04-29 08:14:21 +0200

    Ya instala2

--- a.sh
+++ a.sh
@@ -11,7 +11,7 @@ echo "zsh no está instalado"
 sudo apt install zsh
 fi
 #Ruta al archivo de configuración de Oh My Zsh
-zsh="$HOME/.oh-my-zsh"
+zsh=$(cat ~/.zshrc | grep ZSH_THEME)
  cp ~/tmp/.zshrc ~/.zshrc
  # Verificar si el archivo de configuración de Oh My Zsh existe
 if [ -f "$zsh" ]; then
@@ -21,20 +21,19 @@ echo "Oh My Zsh no está instalado en este sistema."
  git clone https://github.com/ohmyzsh/ohmyzsh.git
  sh ./ohmyzsh/tools/install.sh
 fi
- # Ruta al archivo de configuración de Powerlevel10k
-        p10k="$HOME/.p10k.zsh"
+# Ruta al archivo de configuración de Powerlevel10k
+p10k="$HOME/.p10k.zsh"
         
-        # Verificar si el archivo de configuración de Powerlevel10k existe
-        if [ -f "$p10k" ]; then
-            echo "Powerlevel10k está instalado en este sistema."
-            sudo cp -r ./tmp/.p10k.zsh ~/.p10k.zsh
-        else
-            echo "Powerlevel10k no está instalado en este sistema."
-            git clone https://github.com/romkatv/powerlevel10k.git 
-            echo 'source ~/MiZSH/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-            source ~/.zshrc
-            sudo cp -r ~/tmp/.p10k.zsh ~/.p10k.zsh
-
-        fi
-        sh ~/tmp/batcat.sh
-        sh ~/tmp/lsd.sh
+# Verificar si el archivo de configuración de Powerlevel10k existe
+if [ -f "$p10k" ]; then
+echo "Powerlevel10k está instalado en este sistema."
+sudo cp -r ./tmp/.p10k.zsh ~/.p10k.zsh
+else
+echo "Powerlevel10k no está instalado en este sistema."
+git clone https://github.com/romkatv/powerlevel10k.git 
+echo 'source ~/MiZSH/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+source ~/.zshrc
+sudo cp -r ~/tmp/.p10k.zsh ~/.p10k.zsh
+fi
+sh ~/tmp/batcat.sh
+sh ~/tmp/lsd.sh
