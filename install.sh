aabe34f0745444b852a08f8b127653989f71ffc2
ErTilla <ErTilla69@gmail.com>
2024-05-22 12:17:22 +0200

    pruebas

--- install.sh
+++ install.sh
@@ -10,12 +10,6 @@ else
         echo "searchsploit está instalado después de instalar exploitdb"
     else
         sudo snap install --no-wait searchsploit
-
-        if which searchsploit &>/dev/null; then
-            echo "searchsploit está instalado después de instalar con snap"
-        else
-            echo "La instalación de searchsploit falló"
-        fi
     fi
 fi
 
