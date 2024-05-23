cb268bd83a331c25214be59c083cafce7433b54a
ErTilla <ErTilla69@gmail.com>
2024-05-23 08:41:39 +0200

    restauracion de un sh

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -11,45 +11,5 @@ else
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip 
     unzip Hack.zip
     sudo mv ./*.ttf /usr/share/fonts/
-    #!/bin/bash
-
-# Comprobar si el script se está ejecutando como root
-if [[ $EUID -ne 0 ]]; then
-   echo "Este script debe ejecutarse como root" 
-   exit 1
-fi
-
-# Variables de configuración
-FONTFACE="VGA"
-FONTSIZE="16x32"
-CONFIG_FILE="/etc/default/console-setup"
-
-# Hacer una copia de seguridad del archivo de configuración original
-cp $CONFIG_FILE ${CONFIG_FILE}.bak
-
-# Editar el archivo de configuración de la consola
-sed -i "s/^FONTFACE=.*/FONTFACE=\"$FONTFACE\"/" $CONFIG_FILE
-sed -i "s/^FONTSIZE=.*/FONTSIZE=\"$FONTSIZE\"/" $CONFIG_FILE
-
-# Si las líneas no existen, añadirlas al archivo
-grep -q "^FONTFACE=" $CONFIG_FILE || echo "FONTFACE=\"$FONTFACE\"" >> $CONFIG_FILE
-grep -q "^FONTSIZE=" $CONFIG_FILE || echo "FONTSIZE=\"$FONTSIZE\"" >> $CONFIG_FILE
-
-# Aplicar los cambios
-setupcon
-
-# Confirmar que se han aplicado los cambios
-if [ $? -eq 0 ]; then
-    echo "La configuración de la consola se ha aplicado correctamente."
-else
-    echo "Error al aplicar la configuración de la consola."
-fi
-
-#Reiniciar el sistema (opcional)
- read -p "¿Deseas reiniciar el sistema para aplicar los cambios? (s/n): " answer
- if [[ $answer =~ ^[Ss]$ ]]; then
-     reboot
- fi
-
-
+    fc-cache -fv
 fi
