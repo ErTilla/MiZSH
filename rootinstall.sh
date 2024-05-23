7efd56773d3672dace87fb136ae055afbac79f5a
ErTilla <ErTilla69@gmail.com>
2024-05-23 15:01:39 +0200

    Conseguido

--- rootinstall.sh
+++ rootinstall.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-if which searchsploit &>/dev/null; then
-    echo "searchsploit está instalado"
-else
-    sudo apt -y update
-    sudo apt -y install exploitdb
-
-    if which searchsploit &>/dev/null; then
-        echo "searchsploit está instalado después de instalar exploitdb"
-    else
-        sudo snap install --no-wait searchsploit
-    fi
-fi
-
 zsh_path=$(which zsh)
 
 if [ "$zsh_path" == "/usr/bin/zsh" ]; then
