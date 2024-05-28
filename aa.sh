609723bc4d448f1a0d029de3d7678e91da0c6b07
ErTilla <ErTilla69@gmail.com>
2024-05-28 13:50:25 +0200

    tmux

--- aa.sh
+++ aa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-$ cd
-$ git clone https://github.com/gpakosz/.tmux.git
-$ ln -s -f .tmux/.tmux.conf
-$ cp .tmux/.tmux.conf.local .
-$ git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
-$ mkdir -p "$XDG_CONFIG_HOME/tmux"
-$ ln -s "/path/to/oh-my-tmux/.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
-$ cp "/path/to/oh-my-tmux/.tmux.conf.local" "$XDG_CONFIG_HOME/tmux/tmux.conf.local"
-$ git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
-$ mkdir -p "~/.config/tmux"
-$ ln -s "/path/to/oh-my-tmux/.tmux.conf" "~/.config/tmux/tmux.conf"
-$ cp "/path/to/oh-my-tmux/.tmux.conf.local" "~/.config/tmux/tmux.conf.local"
+ cd
+ git clone https://github.com/gpakosz/.tmux.git
+ ln -s -f .tmux/.tmux.conf
+ cp .tmux/.tmux.conf.local .
+ git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
+ mkdir -p "$XDG_CONFIG_HOME/tmux"
+ ln -s "/path/to/oh-my-tmux/.tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ cp "/path/to/oh-my-tmux/.tmux.conf.local" "$XDG_CONFIG_HOME/tmux/tmux.conf.local"
+ git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
+ mkdir -p "~/.config/tmux"
+ ln -s "/path/to/oh-my-tmux/.tmux.conf" "~/.config/tmux/tmux.conf"
+ cp "/path/to/oh-my-tmux/.tmux.conf.local" "~/.config/tmux/tmux.conf.local"
